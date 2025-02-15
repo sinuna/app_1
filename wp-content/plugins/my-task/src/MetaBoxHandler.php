c3e9b14228acf8e6886b77b895e47f86f2260943
Sinuna Chaudhary <sinunachaudhary@Sinunas-MacBook-Pro.local>
2025-02-15 18:39:56 -0500

    Save highlight meta box

--- wp-content/plugins/my-task/src/MetaBoxHandler.php
+++ wp-content/plugins/my-task/src/MetaBoxHandler.php
@@ -5,6 +5,7 @@ namespace MyTask;
 class MetaBoxHandler {
     public function __construct() {
         add_action('add_meta_boxes', array( $this, 'add_mymeta_boxes'));
+        add_action('save_post', array( $this, 'save_highlight_meta_box'), 10, 2);
     }
 
     public function add_mymeta_boxes() {
@@ -19,11 +20,23 @@ class MetaBoxHandler {
     }
 
     public function render_highlight_meta_box( $post ) {
-    ?>
+        $is_highlight = get_post_meta($post->ID, '_is_highlight', true);
+        $is_highlight = ( empty($is_highlight) ) ? '0' : $is_highlight;
+        ?>
         <label for="highlight">
-            <input type="checkbox" id="highlight" name="highlight" value="1">
+            <input type="checkbox" id="highlight-checkbox" name="highlight-checkbox" value="1" <?php checked($is_highlight, '1');?>>
             Highlight
         </label>
-    <?php
+        <?php
+    }
+
+    public function save_highlight_meta_box( $post_id ) {
+        if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) return $post_id;
+
+        if ( isset( $_POST['highlight-checkbox']) ) {
+            update_post_meta( $post_id, '_is_highlight', '1'); // Set to '1' if checked
+        } else {
+            update_post_meta( $post_id, '_is_highlight', '0' ); // Set to '0' if unchecked
+        }
     }
 }
